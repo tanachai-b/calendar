69922c7cac3d72bc73c9190e85d7424c7855f307
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2023-12-25 15:27:49 +0700

    scroll conversion output with input

--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -3,107 +3,134 @@
 import { useMemo, useState } from "react";
 import { NavBar } from "../components";
 import { monthNames } from "../constants";
+import { initialData } from "./initialData";
 
 export default function Habits() {
-  const initialData = `
-
-2023-12-22
-
-> went on a trip
-
-2023-12-23
-
-- sleep at home
-
-2023-12-24
-
-> go to office
-> have lunch with colleagues
-> go to museum
-
-- 9:00 leave home for office
-^ leave a bit late + traffic jam
-
-- 10:00 arrive at office
-
-- 12:00 have lunch with colleague at department store
-^ continue with ice cream
-
-- 15:00 leave office early, went to the museum
-^ have dinner at museum food court
-
-- 18:00 go home
-
-`.trim();
-
   const [data, setData] = useState(initialData);
 
+  const splitedDays = useMemo(() => splitDays(data), [data]);
   const output = useMemo(() => processData(data), [data]);
+
+  function handleDayChanged(value: string, index: number) {
+    const result = [...splitedDays];
+    result[index] = value;
+    setData(result.join("\n"));
+  }
 
   return (
     <div className="flex flex-col h-screen">
       <NavBar className="border-b border-highlight_yellow" />
 
       <div className="flex flex-row h-full divide-x divide-border overflow-hidden">
-        <div className="flex-1 shrink-0">
+        <div className="flex-1 basis-1/3">
           <textarea
-            className="h-full w-full p-2.5 outline-none text-text_white bg-transparent placeholder:text-text_grey focus:text-text_white focus:bg-bg_hover resize-none"
+            className="h-full w-full p-2.5 outline-none text-text_grey active:text-text_white bg-transparent placeholder:text-text_grey focus:text-text_white focus:bg-bg_hover resize-none"
             placeholder="input"
             value={data}
             onChange={(e) => setData(e.target.value)}
           />
         </div>
 
-        {/* <div className="flex-1 shrink-0 p-2.5 whitespace-pre-wrap font-mono overflow-auto text-text_white">
+        {/* <div className="flex-1 basis-1/3 p-2.5 whitespace-pre-wrap font-mono overflow-auto text-text_grey">
           {JSON.stringify(output, null, 2)}
         </div> */}
 
-        <div className="flex-1 shrink-0 overflow-y-scroll divide-y divide-border">
-          {output.map((v, index) => (
-            <div className="flex flex-col p-2.5 gap-2.5" key={index}>
-              <div className="flex flex-row gap-2.5">
-                <div>{v.year}</div>
-                <div>
-                  {v.month ? monthNames[parseInt(v.month) - 1] : undefined}
-                </div>
-                <div>{v.day}</div>
+        {/* <div className="flex-1 basis-1/3 flex flex-col overflow-y-scroll divide-y divide-border">
+          {output.map((day, index) => (
+            <BeautifiedDay
+              key={index}
+              year={day.year}
+              month={day.month}
+              day={day.day}
+              keypoints={day.keypoints}
+              notes={day.notes}
+            />
+          ))}
+        </div> */}
+
+        <div className="flex-1 basis-2/3 flex flex-col divide-y divide-border overflow-y-auto overflow-x-hidden">
+          {splitedDays.map((day, index) => (
+            <div className="flex flex-row divide-x divide-border" key={index}>
+              <div className="flex-1">
+                <BeautifiedDay
+                  year={output[index].year}
+                  month={output[index].month}
+                  day={output[index].day}
+                  keypoints={output[index].keypoints}
+                  notes={output[index].notes}
+                />
               </div>
 
-              {v.keypoints && v.keypoints.length > 0 ? (
-                <div className="flex flex-wrap gap-1">
-                  {v.keypoints?.map((v, index) => (
-                    <div className="px-1 border border-border" key={index}>
-                      {v}
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <></>
-              )}
+              <div className="flex-1 relative">
+                <textarea
+                  className="peer absolute h-full w-full p-2.5 outline-none text-text_grey active:text-text_white bg-transparent placeholder:text-text_grey focus:text-text_white focus:bg-bg_hover resize-none overflow-hidden"
+                  placeholder="input"
+                  value={day}
+                  onChange={(e) => handleDayChanged(e.target.value, index)}
+                />
 
-              {v.notes && v.notes.length > 0 ? (
-                <div
-                  className="grid grid-cols-[min-content_1fr] gap-2.5"
-                  key={index}
-                >
-                  {v.notes?.map((v) => {
-                    return (
-                      <>
-                        <div className="text-right">{v.time}</div>
-                        <div className="whitespace-pre-wrap">{v.note}</div>
-                      </>
-                    );
-                  })}
+                <div className="flex-1 p-2.5 whitespace-pre-wrap invisible hxidden peer-focus:block">
+                  {day}
+                  {"\n"}
                 </div>
-              ) : (
-                <></>
-              )}
+              </div>
             </div>
           ))}
         </div>
       </div>
     </div>
   );
+
+  function BeautifiedDay({
+    year,
+    month,
+    day,
+    keypoints,
+    notes,
+  }: {
+    year?: string;
+    month?: string;
+    day?: string;
+    keypoints?: string[];
+    notes?: { time?: string; note?: string }[];
+  }) {
+    return (
+      <div className="flex flex-col p-2.5 gap-2.5">
+        <div>
+          <span>{year} </span>
+          <span>{month ? monthNames[parseInt(month) - 1] : undefined} </span>
+          <span>{day}</span>
+        </div>
+
+        {keypoints && keypoints.length > 0 ? (
+          <div className="flex flex-wrap gap-1">
+            {keypoints?.map((v, index) => (
+              <div className="px-1 border border-border" key={index}>
+                {v}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {notes && notes.length > 0 ? (
+          <div className="grid grid-cols-[min-content_1fr] gap-2.5">
+            {notes?.map((v) => {
+              return (
+                <>
+                  <div className="text-right">{v.time}</div>
+                  <div className="whitespace-pre-wrap">{v.note}</div>
+                </>
+              );
+            })}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 }
 
 function processData(data: string) {
@@ -119,12 +146,14 @@ function processData(data: string) {
 }
 
 function splitDays(data: string) {
-  return data
-    .trim()
-    .replace(/^(\d{4}-\d{1,2}-\d{1,2})/gm, "<new_line>$1")
-    .split("<new_line>")
-    .slice(1)
-    .map((value) => value.trim());
+  return (
+    data
+      // .trim()
+      .replace(/\n(\d{4}-\d{1,2}-\d{1,2})/gm, "<new_line>$1")
+      .split("<new_line>")
+    // .slice(1)
+  );
+  // .map((value) => value.trim());
 }
 
 function objectDay(day: string) {
