ee56cc68c0d404f2e1cdbb513d685ecd3952f049
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2023-12-30 09:55:36 +0700

    improve keyboard scheme type

--- app/keyboard/Keyboard.tsx
+++ app/keyboard/Keyboard.tsx
@@ -1,88 +1,141 @@
 import { Key } from "./Key";
 import { qwertyScheme } from "./schemes";
 
-export function Keyboard({
-  scheme = qwertyScheme,
-}: {
-  scheme?: {
+type KeyboardKey =
+  | "`"
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "0"
+  | "-"
+  | "="
+  | "q"
+  | "w"
+  | "e"
+  | "r"
+  | "t"
+  | "y"
+  | "u"
+  | "i"
+  | "o"
+  | "p"
+  | "["
+  | "]"
+  | "\\"
+  | "a"
+  | "s"
+  | "d"
+  | "f"
+  | "g"
+  | "h"
+  | "j"
+  | "k"
+  | "l"
+  | ";"
+  | "'"
+  | "z"
+  | "x"
+  | "c"
+  | "v"
+  | "b"
+  | "n"
+  | "m"
+  | ","
+  | "."
+  | "/";
+
+export type KeyboardScheme = {
+  [Key in KeyboardKey]?: {
     leftTop?: string;
     leftBottom?: string;
     rightTop?: string;
     rightBottom?: string;
-  }[];
+  };
+};
+
+export function Keyboard({
+  scheme = qwertyScheme,
+}: {
+  scheme?: KeyboardScheme;
 }) {
   return (
     <div className="border border-border rounded-xl overflow-hidden">
       <div className="flex flex-col divide-y divide-border">
         <div className="flex flex-row divide-x divide-border">
-          <Key {...scheme[0]} className="rounded-tl-xl" />
+          <Key {...scheme["`"]} className="rounded-tl-xl" />
 
-          <Key {...scheme[1]} />
-          <Key {...scheme[2]} />
-          <Key {...scheme[3]} />
-          <Key {...scheme[4]} />
-          <Key {...scheme[5]} />
-          <Key {...scheme[6]} />
-          <Key {...scheme[7]} />
-          <Key {...scheme[8]} />
-          <Key {...scheme[9]} />
-          <Key {...scheme[10]} />
+          <Key {...scheme["1"]} />
+          <Key {...scheme["2"]} />
+          <Key {...scheme["3"]} />
+          <Key {...scheme["4"]} />
+          <Key {...scheme["5"]} />
+          <Key {...scheme["6"]} />
+          <Key {...scheme["7"]} />
+          <Key {...scheme["8"]} />
+          <Key {...scheme["9"]} />
+          <Key {...scheme["0"]} />
 
-          <Key {...scheme[11]} />
-          <Key {...scheme[12]} />
+          <Key {...scheme["-"]} />
+          <Key {...scheme["="]} />
           <Key center="Backspace" width={2} className="rounded-tr-xl" />
         </div>
 
         <div className="flex flex-row divide-x divide-border">
           <Key center="Tab" width={1.5} />
 
-          <Key {...scheme[13]} />
-          <Key {...scheme[14]} />
-          <Key {...scheme[15]} />
-          <Key {...scheme[16]} />
-          <Key {...scheme[17]} />
-          <Key {...scheme[18]} />
-          <Key {...scheme[19]} />
-          <Key {...scheme[20]} />
-          <Key {...scheme[21]} />
-          <Key {...scheme[22]} />
+          <Key {...scheme["q"]} />
+          <Key {...scheme["w"]} />
+          <Key {...scheme["e"]} />
+          <Key {...scheme["r"]} />
+          <Key {...scheme["t"]} />
+          <Key {...scheme["y"]} />
+          <Key {...scheme["u"]} />
+          <Key {...scheme["i"]} />
+          <Key {...scheme["o"]} />
+          <Key {...scheme["p"]} />
 
-          <Key {...scheme[23]} />
-          <Key {...scheme[24]} />
-          <Key {...scheme[25]} width={1.5} />
+          <Key {...scheme["["]} />
+          <Key {...scheme["]"]} />
+          <Key {...scheme["\\"]} width={1.5} />
         </div>
 
         <div className="flex flex-row divide-x divide-border">
           <Key center="Caps" width={1.75} />
 
-          <Key {...scheme[26]} />
-          <Key {...scheme[27]} />
-          <Key {...scheme[28]} />
-          <Key {...scheme[29]} isTactile={true} />
-          <Key {...scheme[30]} />
-          <Key {...scheme[31]} />
-          <Key {...scheme[32]} isTactile={true} />
-          <Key {...scheme[33]} />
-          <Key {...scheme[34]} />
-          <Key {...scheme[35]} />
+          <Key {...scheme["a"]} />
+          <Key {...scheme["s"]} />
+          <Key {...scheme["d"]} />
+          <Key {...scheme["f"]} isTactile={true} />
+          <Key {...scheme["g"]} />
+          <Key {...scheme["h"]} />
+          <Key {...scheme["j"]} isTactile={true} />
+          <Key {...scheme["k"]} />
+          <Key {...scheme["l"]} />
+          <Key {...scheme[";"]} />
 
-          <Key {...scheme[36]} />
+          <Key {...scheme["'"]} />
           <Key center="Enter" width={2.25} />
         </div>
 
         <div className="flex flex-row divide-x divide-border">
           <Key center="Shift" width={2.25} />
 
-          <Key {...scheme[37]} />
-          <Key {...scheme[38]} />
-          <Key {...scheme[39]} />
-          <Key {...scheme[40]} />
-          <Key {...scheme[41]} />
-          <Key {...scheme[42]} />
-          <Key {...scheme[43]} />
-          <Key {...scheme[44]} />
-          <Key {...scheme[45]} />
-          <Key {...scheme[46]} />
+          <Key {...scheme["z"]} />
+          <Key {...scheme["x"]} />
+          <Key {...scheme["c"]} />
+          <Key {...scheme["v"]} />
+          <Key {...scheme["b"]} />
+          <Key {...scheme["n"]} />
+          <Key {...scheme["m"]} />
+          <Key {...scheme[","]} />
+          <Key {...scheme["."]} />
+          <Key {...scheme["/"]} />
 
           <Key center="Shift" width={2.75} />
         </div>
