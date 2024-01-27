595617d3a86038b56c4a06c8527454133a35225e
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-01-28 00:27:22 +0700

    complete moon dial

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -323,33 +323,56 @@ export default function Draw() {
                 <circle cx={0} cy={0} r={80} fill="none" />
                 {/* <circle cx={0} cy={0} r={80 - 30} fill="none" /> */}
 
-                {Array.from({ length: 4 }).map((v, i) => (
+                {Array.from({ length: 8 }).map((v, i) => (
                   <line
                     key={i}
                     x1={0}
                     y1={-80}
                     x2={0}
                     y2={-80 + 30}
-                    strokeWidth="7"
-                    transform={`rotate(${(i / 4) * 360}, 0, 0)`}
+                    transform={`rotate(${((i + 0.5) / 8) * 360}, 0, 0)`}
                   />
                 ))}
 
-                {Array.from({ length: 28 }).map((v, i) => (
-                  <line
-                    key={i}
-                    x1={0}
-                    y1={-80}
-                    x2={0}
-                    y2={-80 + 10}
-                    transform={`rotate(${(i / 28) * 360}, 0, 0)`}
-                  />
+                {[
+                  "\uD83C\uDF15",
+                  "\uD83C\uDF16",
+                  "\uD83C\uDF17",
+                  "\uD83C\uDF18",
+                  "\uD83C\uDF11",
+                  "\uD83C\uDF12",
+                  "\uD83C\uDF13",
+                  "\uD83C\uDF14",
+                ].map((v, i) => (
+                  <text key={i} transform={`rotate(${(-i / 8) * 360})`}>
+                    <textPath
+                      strokeWidth={0}
+                      fill={i === 0 ? "#e0e0e0" : "#e0e0e0"}
+                      className={cx("text-x20", "font-bold")}
+                      textAnchor={"middle"}
+                      startOffset="50%"
+                      alignmentBaseline={"central"}
+                      href={"#textpath"}
+                    >
+                      {/* {i + 1} */}
+                      {v + "\uFE0E"}
+                    </textPath>
+                  </text>
                 ))}
 
                 <g
                   fill="#c00000"
                   stroke="none"
-                  transform={`rotate(${135})`}
+                  transform={`rotate(${
+                    -(
+                      (new Date() - new Date(2024, 0, 11, 18, 57)) /
+                      1000 /
+                      60 /
+                      60 /
+                      24 /
+                      29.53059
+                    ) * 360
+                  })`}
                   filter="url(#shadow1)"
                 >
                   <polygon
