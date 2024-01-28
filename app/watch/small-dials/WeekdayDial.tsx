77016006b458a0813e9a33f30e4b3510c0b694dd
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-01-29 00:35:37 +0700

    improve weekday dial hand

--- app/watch/small-dials/WeekdayDial.tsx
+++ app/watch/small-dials/WeekdayDial.tsx
@@ -3,6 +3,8 @@
 import cx from "classnames";
 
 export function WeekdayDial({ value }: { value: number }) {
+  const angle = -((new Date().getDay() + value / 360 / 60 / 24) / 7) * 360;
+
   return (
     <g transform={`translate(${125}, ${0})`}>
       <circle r={80} fill="url(#radial1)" />
@@ -42,38 +44,96 @@ export function WeekdayDial({ value }: { value: number }) {
         );
       })}
 
-      <g
-        transform={`rotate(${
-          -((new Date().getDay() + value / 360 / 60 / 24) / 7) * 360
-        })`}
-      >
-        <g fill="#808080" filter="url(#shadow1)">
-          <polygon
-            points={cx(
-              `${-15 / 2},${-47}`,
-              `${0},${-55}`,
-              `${15 / 2},${-47}`,
-              `${30 / 2},${0}`,
-              `${-30 / 2},${0}`,
-              `${-15 / 2},${-47}`
-            )}
-          />
+      <SmallDialHand angle={angle} />
+    </g>
+  );
+}
 
-          <circle r={30 / 2} />
-        </g>
-
+export function SmallDialHand({ angle }: { angle: number }) {
+  return (
+    <g filter="url(#shadow1)">
+      <defs>
         <polygon
-          fill="#e0a000"
+          id="weekday-dial-hand"
           points={cx(
             `${-15 / 2},${-47}`,
             `${0},${-55}`,
             `${15 / 2},${-47}`,
-            `${20 / 2},${-30}`,
-            `${-20 / 2},${-30}`,
+            `${30 / 2},${0}`,
+            `${-30 / 2},${0}`,
             `${-15 / 2},${-47}`
           )}
+          transform={`rotate(${angle})`}
         />
+
+        <circle id="weekday-dial-circle" r={30 / 2} />
+
+        <g id="weekday-dial-shape">
+          <use href="#weekday-dial-hand" />
+          <use href="#weekday-dial-circle" />
+        </g>
+
+        <clipPath id="weekday-dial-clip">
+          <use href="#weekday-dial-hand" />
+          <use href="#weekday-dial-circle" />
+        </clipPath>
+
+        <filter id="erode">
+          <feMorphology operator="erode" radius="1.25" />
+        </filter>
+
+        <filter id="blur">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
+        </filter>
+      </defs>
+
+      <g>
+        <use href="#weekday-dial-shape" fill="#808080" />
+
+        <g fill="#e0a000" clipPath="url(#weekday-dial-clip)">
+          <rect
+            x={-50 / 2}
+            y={-55}
+            width={50}
+            height={20}
+            transform={`rotate(${angle})`}
+          />
+        </g>
       </g>
+
+      <mask id="weekday-dial-light">
+        <use
+          href="#weekday-dial-shape"
+          fill="#ffffff"
+          filter="url(#erode) url(#blur)"
+          transform="translate(-2,-2)"
+        />
+
+        <use href="#weekday-dial-shape" fill="#000000" filter="url(#erode)" />
+      </mask>
+
+      <mask id="weekday-dial-shadow">
+        <use
+          href="#weekday-dial-shape"
+          fill="#ffffff"
+          filter="url(#erode) url(#blur)"
+          transform="translate(2,2)"
+        />
+
+        <use href="#weekday-dial-shape" fill="#000000" filter="url(#erode)" />
+      </mask>
+
+      <use
+        href="#weekday-dial-shape"
+        fill="#ffffff80"
+        mask="url(#weekday-dial-light)"
+      />
+
+      <use
+        href="#weekday-dial-shape"
+        fill="#00000080"
+        mask="url(#weekday-dial-shadow)"
+      />
     </g>
   );
 }
