de66a8e39efab68f9d92f72c63c79f034063468c
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-01-30 01:15:43 +0700

    refactor coarse scale

--- app/watch/watch-scales/CoarseScale.tsx
+++ app/watch/watch-scales/CoarseScale.tsx
@@ -3,47 +3,50 @@
 import { Bevel } from "../Bevel";
 
 export function CoarseScale() {
-  const rand = Math.floor(Math.random() * 36 ** 4).toString(36);
-
   return (
     <g filter="url(#shadow2)">
-      <defs>
-        <g id={`shape-${rand}`}>
-          {Array.from({ length: 12 }).map((v, i) => {
-            if (i === 0) return;
-            return (
-              <rect
-                key={i}
-                x={-15 / 2}
-                y={-230}
-                width={15}
-                height={i % 3 === 0 ? 20 : 50}
-                transform={`rotate(${(i / 12) * 360}, 0, 0)`}
-              />
-            );
-          })}
+      {Array.from({ length: 12 }).map((v, i) => {
+        const rand = Math.floor(Math.random() * 36 ** 4).toString(36);
 
-          <rect
-            x={-15 / 2}
-            y={-230}
-            width={15}
-            height={50}
-            transform={`translate(-12,0)`}
-          />
+        return (
+          <g key={i}>
+            <defs>
+              {i !== 0 ? (
+                <rect
+                  id={`shape-${rand}`}
+                  x={-15 / 2}
+                  y={-230}
+                  width={15}
+                  height={i % 3 === 0 ? 20 : 50}
+                  transform={`rotate(${(i / 12) * 360}, 0, 0)`}
+                />
+              ) : (
+                <g id={`shape-${rand}`}>
+                  <rect
+                    x={-15 / 2}
+                    y={-230}
+                    width={15}
+                    height={50}
+                    transform={`translate(-12,0)`}
+                  />
 
-          <rect
-            x={-15 / 2}
-            y={-230}
-            width={15}
-            height={50}
-            transform={`translate(12,0)`}
-          />
-        </g>
-      </defs>
+                  <rect
+                    x={-15 / 2}
+                    y={-230}
+                    width={15}
+                    height={50}
+                    transform={`translate(12,0)`}
+                  />
+                </g>
+              )}
+            </defs>
 
-      <use href={`#shape-${rand}`} fill="#e0a000" />
+            <use href={`#shape-${rand}`} fill="#e0a000" />
 
-      <Bevel shape={<use href={`#shape-${rand}`} />} />
+            <Bevel shape={<use href={`#shape-${rand}`} />} />
+          </g>
+        );
+      })}
     </g>
   );
 }
