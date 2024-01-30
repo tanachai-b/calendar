4350918a955a2da80c88f70929bb1de0c921e53d
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-01-30 23:55:24 +0700

    refactor flip number

--- app/digital-clock/page.tsx
+++ app/digital-clock/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import cx from "classnames";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import { NavBar } from "../components";
 
@@ -35,7 +35,10 @@ function FlipNumber() {
     <div style={{ transform: "scale(1.5)" }}>
       <div
         className={cx("size-fit", "w-[150px]", "h-[200px]")}
-        style={{ perspective: "500px" }}
+        style={{
+          perspective: "500px",
+          perspectiveOrigin: "75px 100px",
+        }}
       >
         <div
           className={cx("absolute", "w-[150px]", "h-[200px]", "rounded-x20")}
@@ -56,24 +59,7 @@ function FlipNumber() {
               "overflow-hidden"
             )}
           >
-            <div
-              className={cx(
-                "absolute",
-                "w-[150px]",
-                "h-[200px]",
-                "top-x0",
-
-                "flex",
-                "items-center",
-                "justify-center",
-
-                "text-white-dark",
-                "text-[200px]",
-                "font-semibold"
-              )}
-            >
-              5
-            </div>
+            <Label className="top-x0">5</Label>
           </div>
         </div>
 
@@ -96,25 +82,33 @@ function FlipNumber() {
               "overflow-hidden"
             )}
           >
-            <div
-              className={cx(
-                "absolute",
-                "w-[150px]",
-                "h-[200px]",
-                "bottom-x0",
-
-                "flex",
-                "items-center",
-                "justify-center",
-
-                "text-white-dark",
-                "text-[200px]",
-                "font-semibold"
-              )}
-            >
-              4
-            </div>
+            <Label className="bottom-x0">4</Label>
           </div>
+        </div>
+
+        <div
+          className={cx(
+            "absolute",
+            "w-[100%]",
+            "h-[calc(50%-1px)]",
+            "top-x0",
+
+            "bg-black-light",
+            "rounded-t-x20",
+
+            "border",
+            "border-grey-dark",
+
+            "overflow-hidden"
+          )}
+          style={{
+            transform: `rotate3d(1, 0, 0, ${
+              value <= 0 && value > -90 ? value : 90
+            }deg)`,
+            transformOrigin: "75px 100px",
+          }}
+        >
+          <Label className="top-x0">4</Label>
         </div>
 
         <div
@@ -128,8 +122,8 @@ function FlipNumber() {
           <div
             className={cx(
               "absolute",
-              "w-[150px]",
-              "h-[99px]",
+              "w-[100%]",
+              "h-[calc(50%-1px)]",
               "bottom-x0",
 
               "bg-black-light",
@@ -141,72 +135,40 @@ function FlipNumber() {
               "overflow-hidden"
             )}
           >
-            <div
-              className={cx(
-                "absolute",
-                "w-[150px]",
-                "h-[200px]",
-                "bottom-x0",
-
-                "flex",
-                "items-center",
-                "justify-center",
-
-                "text-white-dark",
-                "text-[200px]",
-                "font-semibold"
-              )}
-            >
-              5
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={cx("absolute", "w-[150px]", "h-[200px]", "rounded-x20")}
-          style={{
-            transform: `rotate3d(1, 0, 0, ${
-              value <= 0 && value > -90 ? value : 90
-            }deg)`,
-          }}
-        >
-          <div
-            className={cx(
-              "absolute",
-              "w-[150px]",
-              "h-[99px]",
-              "top-x0",
-
-              "bg-black-light",
-              "rounded-t-x20",
-
-              "border",
-              "border-grey-dark",
-
-              "overflow-hidden"
-            )}
-          >
-            <div
-              className={cx(
-                "absolute",
-                "w-[150px]",
-                "h-[200px]",
-                "top-x0",
-
-                "flex",
-                "items-center",
-                "justify-center",
-
-                "text-white-dark",
-                "text-[200px]",
-                "font-semibold"
-              )}
-            >
-              4
-            </div>
+            <Label className="bottom-x0">5</Label>
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Label({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: ReactNode;
+}) {
+  return (
+    <div
+      className={cx(
+        "absolute",
+        "w-[100%]",
+        "h-[200%]",
+
+        "flex",
+        "items-center",
+        "justify-center",
+
+        "text-white-dark",
+        "text-[200px]",
+        "font-semibold",
+
+        className
+      )}
+    >
+      {children}
     </div>
   );
 }
