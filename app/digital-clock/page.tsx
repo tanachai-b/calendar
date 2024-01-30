fb6aea6bd4b5f51ea4844b3d989bd3945a0db8f8
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-01-31 00:11:05 +0700

    refactor flip number

--- app/digital-clock/page.tsx
+++ app/digital-clock/page.tsx
@@ -32,113 +32,81 @@ function FlipNumber() {
   }, []);
 
   return (
-    <div style={{ transform: "scale(1.5)" }}>
+    <div style={{ transform: "scale(1)" }}>
       <div
-        className={cx("size-fit", "w-[150px]", "h-[200px]")}
+        className={cx("size-fit", "w-[200px]", "h-[250px]")}
         style={{
-          perspective: "500px",
-          perspectiveOrigin: "75px 100px",
+          perspective: "700px",
+          perspectiveOrigin: "50% 50%",
         }}
       >
-        <div
-          className={cx("absolute", "w-[150px]", "h-[200px]", "rounded-x20")}
-        >
-          <div
-            className={cx(
-              "absolute",
-              "w-[150px]",
-              "h-[99px]",
-              "top-x0",
-
-              "bg-black-light",
-              "rounded-t-x20",
-
-              "border",
-              "border-grey-dark",
-
-              "overflow-hidden"
-            )}
-          >
+        <div className={cx("absolute", "w-[100%]", "h-[100%]")}>
+          <Flap className={cx("top-x0", "rounded-t-x15")}>
             <Label className="top-x0">5</Label>
-          </div>
+          </Flap>
         </div>
 
-        <div
-          className={cx("absolute", "w-[150px]", "h-[200px]", "rounded-x20")}
-        >
-          <div
-            className={cx(
-              "absolute",
-              "w-[150px]",
-              "h-[99px]",
-              "bottom-x0",
-
-              "bg-black-light",
-              "rounded-b-x20",
-
-              "border",
-              "border-grey-dark",
-
-              "overflow-hidden"
-            )}
-          >
+        <div className={cx("absolute", "w-[100%]", "h-[100%]")}>
+          <Flap className={cx("bottom-x0", "rounded-b-x15")}>
             <Label className="bottom-x0">4</Label>
-          </div>
+          </Flap>
         </div>
 
         <div
-          className={cx(
-            "absolute",
-            "w-[100%]",
-            "h-[calc(50%-1px)]",
-            "top-x0",
-
-            "bg-black-light",
-            "rounded-t-x20",
-
-            "border",
-            "border-grey-dark",
-
-            "overflow-hidden"
-          )}
+          className={cx("absolute", "w-[100%]", "h-[100%]")}
           style={{
             transform: `rotate3d(1, 0, 0, ${
               value <= 0 && value > -90 ? value : 90
             }deg)`,
-            transformOrigin: "75px 100px",
           }}
         >
-          <Label className="top-x0">4</Label>
+          <Flap className={cx("top-x0", "rounded-t-x15")}>
+            <Label className="top-x0">4</Label>
+          </Flap>
         </div>
 
         <div
-          className={cx("absolute", "w-[150px]", "h-[200px]", "rounded-x20")}
+          className={cx("absolute", "w-[100%]", "h-[100%]")}
           style={{
             transform: `rotate3d(1, 0, 0, ${
               value <= -90 && value > -180 ? value - 180 : 90
             }deg)`,
           }}
         >
-          <div
-            className={cx(
-              "absolute",
-              "w-[100%]",
-              "h-[calc(50%-1px)]",
-              "bottom-x0",
-
-              "bg-black-light",
-              "rounded-b-x20",
-
-              "border",
-              "border-grey-dark",
-
-              "overflow-hidden"
-            )}
-          >
+          <Flap className={cx("bottom-x0", "rounded-b-x15")}>
             <Label className="bottom-x0">5</Label>
-          </div>
+          </Flap>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Flap({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: ReactNode;
+}) {
+  return (
+    <div
+      className={cx(
+        "absolute",
+        "w-[100%]",
+        "h-[calc(50%-2px)]",
+
+        "bg-[#101010]",
+
+        "border",
+        "border-[#202020]",
+
+        "overflow-hidden",
+
+        className
+      )}
+    >
+      {children}
     </div>
   );
 }
@@ -161,8 +129,8 @@ function Label({
         "items-center",
         "justify-center",
 
-        "text-white-dark",
-        "text-[200px]",
+        "text-[#e0e0e0]",
+        "text-[270px]",
         "font-semibold",
 
         className
