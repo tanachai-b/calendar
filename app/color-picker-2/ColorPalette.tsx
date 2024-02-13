6430742a6226017dc77db6540b85be54b89eb5df
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-02-14 00:09:23 +0700

    dynamic border color

--- app/color-picker-2/ColorPalette.tsx
+++ app/color-picker-2/ColorPalette.tsx
@@ -23,20 +23,30 @@ export function ColorPalette({
 
 function ColorCard({ color }: { color: string }) {
   return (
-    <div className={cx("size-x70", "bg-black", "p-x1")}>
+    <div
+      className={cx(
+        "size-x70"
+        //  "bg-black",
+        //  "p-x1"
+      )}
+    >
       <div
         className={cx(
           "size-full",
 
           "border-x5",
-          "border-white",
+          // "border-black",
 
           "p-x2",
           "text-x10",
           "font-semibold",
           "leading-none"
         )}
-        style={{ background: color, color: `${textColor(color)}ff` }}
+        style={{
+          background: color,
+          borderColor: textColor(color),
+          color: `${textColor(color)}ff`,
+        }}
       >
         {color.toUpperCase().slice(1)}
       </div>
@@ -49,5 +59,9 @@ function textColor(color: string) {
   const g = parseInt(color.slice(3, 5), 16);
   const b = parseInt(color.slice(5, 7), 16);
 
-  return r * 0.75 + g * 2 + b * 0.25 > 128 * 3 ? "#000000" : "#ffffff";
+  const [rf, gf, bf] = [1, 2, 1];
+
+  return r * rf + g * gf + b * bf > 256 * (rf + gf + bf) * (4 / 8)
+    ? "#000000"
+    : "#ffffff";
 }
