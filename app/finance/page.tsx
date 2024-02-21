20d17af395200308f25064bbafc0a6f40b2c1076
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-02-22 02:15:04 +0700

    fix finance page padding

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -22,8 +22,9 @@ export default function Finance() {
           "text-[#000000ff]"
         )}
       >
-        <AccountsCard accounts={accounts} />
         <div className={cx("flex", "flex-col", "p-x20", "gap-x20")}>
+          <AccountsCard accounts={accounts} />
+
           <BanksCard />
         </div>
       </div>
